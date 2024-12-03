f852b050106740e3ef16bb31c48e5ec9498311b5
Thomas <tholeguen@ik.me>
2024-12-03 21:40:42 +0100

    init

---
+++ walletweb.nix
@@ -0,0 +1,29 @@
+{ lib, stdenv, python3, python3Packages, fetchPypi, sqlite }:
+let
+  fs = lib.fileset;
+  sourceFiles = fs.unions [
+    ./app.py
+    ./job.py
+    ./static
+    ./templates
+  ];
+in
+
+with python3Packages;
+buildPythonApplication {
+  pname = "walletweb";
+  version = "1.0";
+
+  src = fs.toSource {
+    root = ./.;
+    fileset = sourceFiles;
+  };
+
+  buildInputs = [ python3 flask schedule sqlite ];
+
+  meta = with lib; {
+    description = "A simple Python app using Flask, Schedule, and SQLite";
+    license = licenses.mit;
+    maintainers = with maintainers; [ "Thomas LE GUEN" ]; # Replace with your name
+  };
+}
